bf98b7d048bc405537589952cb1ba2cfa15fb8a4
shahin2525 <shahin451999@gmail.com>
2024-06-26 09:55:44 -0700

    practice object type

---
+++ module1/src/1.1.ts
@@ -0,0 +1,12 @@
+const user: {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  isMarried: boolean;
+  readonly company: string;
+} = {
+  firstName: "rakib",
+  lastName: "sakib",
+  isMarried: true,
+  company: "ph",
+};
