6b0217c1e4928e037dc07e996a186fdb235fedf6
shahin2525 <shahin451999@gmail.com>
2024-06-26 12:40:40 -0700

    practice destructuring

---
+++ module1/src/1.4.ts
@@ -0,0 +1,21 @@
+{
+  const user = {
+    id: "a444",
+    name: {
+      firstName: "md",
+      middleName: "rakib",
+      lastName: "khan",
+    },
+    age: 16,
+  };
+
+  const {
+    id,
+    age,
+    name: { firstName, middleName, lastName: endName },
+  } = user;
+  //   console.log(endName);
+  const array1 = ["sakib", "musi", "rusi", "chusi", "kushi"];
+  const [, , ru, ...next] = array1;
+  console.log(ru, next);
+}
