0212e8a772a25587bd7e082aee2b02798ec64237
shahin2525 <shahin451999@gmail.com>
2024-06-25 22:32:37 -0700

    first commit

---
+++ module1/src/index.ts
@@ -0,0 +1,1 @@
+let course: string = "next level web development";
---
+++ module1/src/text.ts
@@ -0,0 +1,9 @@
+let d: number = 1;
+let x: string = "sdeed";
+let b: boolean = true;
+let u: undefined = undefined;
+let n: null = null;
+// non-primitive
+let friends: string[] = ["rkaib", "fa"];
+let numbers: number[] = [10, 20, 30];
+let tupple: [string, number, boolean] = ["raki", 10, true];
